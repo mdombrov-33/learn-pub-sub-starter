4ad5e0dfcaf630c562452e0b5aa5d36b0e7e7b6c
Maksym Dombrov <maksymdombrov@gmail.com>
2025-11-22 17:14:34 +0200

    upd

--- rabbit.sh
+++ rabbit.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+CONTAINER_NAME="peril_rabbitmq"
+IMAGE_NAME="rabbitmq-stomp"
+
 start_or_run () {
-    docker inspect peril_rabbitmq > /dev/null 2>&1
+    docker inspect $CONTAINER_NAME > /dev/null 2>&1
 
     if [ $? -eq 0 ]; then
         echo "Starting Peril RabbitMQ container..."
-        docker start peril_rabbitmq
+        docker start $CONTAINER_NAME
     else
         echo "Peril RabbitMQ container not found, creating a new one..."
-        docker run -d --name peril_rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        docker run -d --name $CONTAINER_NAME -p 5672:5672 -p 15672:15672 -p 61613:61613 $IMAGE_NAME
     fi
 }
 
@@ -18,11 +21,11 @@ case "$1" in
         ;;
     stop)
         echo "Stopping Peril RabbitMQ container..."
-        docker stop peril_rabbitmq
+        docker stop $CONTAINER_NAME
         ;;
     logs)
         echo "Fetching logs for Peril RabbitMQ container..."
-        docker logs -f peril_rabbitmq
+        docker logs -f $CONTAINER_NAME
         ;;
     *)
         echo "Usage: $0 {start|stop|logs}"
