b569160d336787c63a39e20e10b6ce572813c2c8
Maksym Dombrov <maksymdombrov@gmail.com>
2025-11-23 20:56:30 +0200

    upd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -20,71 +19,19 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
 
-	publishCh, err := conn.Channel()
-	if err != nil {
-		log.Fatalf("could not create channel: %v", err)
-	}
-
 	err = pubsub.SubscribeGob(
 		conn,
 		routing.ExchangePerilTopic,
 		routing.GameLogSlug,
 		routing.GameLogSlug+".*",
 		pubsub.SimpleQueueDurable,
-		func(gamelog routing.GameLog) pubsub.Acktype {
-			defer gamelogic.PrintServerHelp()
-			err := gamelogic.WriteLog(gamelog)
-			if err != nil {
-				fmt.Printf("could not write log: %v\n", err)
-				return pubsub.NackRequeue
-			}
-			return pubsub.Ack
-		},
+		handlerLogs(),
 	)
 	if err != nil {
-		log.Fatalf("could not subscribe to game logs: %v", err)
+		log.Fatalf("could not starting consuming logs: %v", err)
 	}
-	fmt.Println("Subscribed to game logs!")
+	fmt.Println("Game server connected! Consuming logs...")
 
-	gamelogic.PrintServerHelp()
-
-	for {
-		words := gamelogic.GetInput()
-		if len(words) == 0 {
-			continue
-		}
-		switch words[0] {
-		case "pause":
-			fmt.Println("Publishing paused game state")
-			err = pubsub.PublishJSON(
-				publishCh,
-				routing.ExchangePerilDirect,
-				routing.PauseKey,
-				routing.PlayingState{
-					IsPaused: true,
-				},
-			)
-			if err != nil {
-				log.Printf("could not publish time: %v", err)
-			}
-		case "resume":
-			fmt.Println("Publishing resumes game state")
-			err = pubsub.PublishJSON(
-				publishCh,
-				routing.ExchangePerilDirect,
-				routing.PauseKey,
-				routing.PlayingState{
-					IsPaused: false,
-				},
-			)
-			if err != nil {
-				log.Printf("could not publish time: %v", err)
-			}
-		case "quit":
-			log.Println("goodbye")
-			return
-		default:
-			fmt.Println("unknown command")
-		}
-	}
+	// Run forever, consuming logs
+	select {}
 }
--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -32,49 +32,19 @@ func SubscribeJSON[T any](
 	queueType SimpleQueueType,
 	handler func(T) Acktype,
 ) error {
-	ch, queue, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
-	if err != nil {
-		return fmt.Errorf("could not declare and bind queue: %v", err)
-	}
-
-	msgs, err := ch.Consume(
-		queue.Name, // queue
-		"",         // consumer
-		false,      // auto-ack
-		false,      // exclusive
-		false,      // no-local
-		false,      // no-wait
-		nil,        // args
+	return subscribe[T](
+		conn,
+		exchange,
+		queueName,
+		key,
+		queueType,
+		handler,
+		func(data []byte) (T, error) {
+			var target T
+			err := json.Unmarshal(data, &target)
+			return target, err
+		},
 	)
-	if err != nil {
-		return fmt.Errorf("could not consume messages: %v", err)
-	}
-
-	unmarshaller := func(data []byte) (T, error) {
-		var target T
-		err := json.Unmarshal(data, &target)
-		return target, err
-	}
-
-	go func() {
-		defer ch.Close()
-		for msg := range msgs {
-			target, err := unmarshaller(msg.Body)
-			if err != nil {
-				fmt.Printf("could not unmarshal message: %v\n", err)
-				continue
-			}
-			switch handler(target) {
-			case Ack:
-				msg.Ack(false)
-			case NackDiscard:
-				msg.Nack(false, false)
-			case NackRequeue:
-				msg.Nack(false, true)
-			}
-		}
-	}()
-	return nil
 }
 
 func SubscribeGob[T any](
@@ -85,9 +55,40 @@ func SubscribeGob[T any](
 	queueType SimpleQueueType,
 	handler func(T) Acktype,
 ) error {
+	return subscribe[T](
+		conn,
+		exchange,
+		queueName,
+		key,
+		queueType,
+		handler,
+		func(data []byte) (T, error) {
+			buffer := bytes.NewBuffer(data)
+			decoder := gob.NewDecoder(buffer)
+			var target T
+			err := decoder.Decode(&target)
+			return target, err
+		},
+	)
+}
+
+func subscribe[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+	handler func(T) Acktype,
+	unmarshaller func([]byte) (T, error),
+) error {
 	ch, queue, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
 	if err != nil {
 		return fmt.Errorf("could not declare and bind queue: %v", err)
+	}
+
+	err = ch.Qos(10, 0, false)
+	if err != nil {
+		return fmt.Errorf("could not set QoS: %v", err)
 	}
 
 	msgs, err := ch.Consume(
@@ -101,14 +102,6 @@ func SubscribeGob[T any](
 	)
 	if err != nil {
 		return fmt.Errorf("could not consume messages: %v", err)
-	}
-
-	unmarshaller := func(data []byte) (T, error) {
-		var target T
-		buf := bytes.NewBuffer(data)
-		decoder := gob.NewDecoder(buf)
-		err := decoder.Decode(&target)
-		return target, err
 	}
 
 	go func() {
